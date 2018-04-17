b31e1fe925127a2acf7515550775d9fddb211b00
Arrow <arrow9867@gmail.com>
2018-04-17 13:32:36 +0930

    Create citizen.lua

---
+++ gamemode/factions/citizen.lua
@@ -0,0 +1,6 @@
+factionName = "Citizen"
+factionDesc = "dont break the law"
+factionColour = Colour(255, 255, 255)
+factionModel = {"models/player/group01/male_02.mdl"}
+
+factionIndex = FACTION_CITIZEN
