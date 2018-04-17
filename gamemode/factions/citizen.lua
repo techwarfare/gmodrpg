81eb5a7843ba32dd540babbf17a1cd934ccc279e
Arrow <arrow9867@gmail.com>
2018-04-17 13:41:49 +0930

    Update citizen.lua

--- gamemode/factions/citizen.lua
+++ gamemode/factions/citizen.lua
@@ -2,5 +2,6 @@ factionName = "Citizen"
 factionDesc = "dont break the law"
 factionColour = Colour(255, 255, 255)
 factionModel = {"models/player/group01/male_02.mdl"}
+factionWeapons = {"weapon_smg1"}
 
 factionIndex = FACTION_CITIZEN
